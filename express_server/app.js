d1af31876c62f1610cd57033d1d86373c0b5983d
faizulislamfair <faizulislamfair@gmail.com>
2022-05-01 18:02:07 +0600

    http methods

--- express_server/app.js
+++ express_server/app.js
@@ -2,5 +2,27 @@ const express = require('express')
 const app = express()
 
 
+
+app.get("/", (req, res)=>{
+    res.send('This is a get request at home route');
+})
+
+app.get("/about", (req, res)=>{
+    res.send('This is a get request at about route');
+})
+
+app.post("/", (req, res)=>{
+    res.send('This is a post request at home route');
+})
+
+app.put("/", (req, res)=>{
+    res.send('This is a put request at home route');
+})
+
+app.delete("/", (req, res)=>{
+    res.send('This is a delete request at home route');
+})
+
+
 module.exports = app;
 
