d5740da2612a957eacea3ad4d0bd587152355e4f
faizulislamfair <faizulislamfair@gmail.com>
2022-05-04 07:09:59 +0600

    make post request with JSON data

---
+++ request_methods/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+
+// Query Parameter
+
+// app.get("/", (req,res)=>{
+    // const id = req.query.id;
+    // const name = req.query.name;
+
+    //destructure
+    // const {id, name} = req.query;
+    // res.send(`<h1>Student id is: ${id} & and name is: ${name}</h1>`);
+// })
+
+
+
+// Route Parameter
+
+// app.get("/userId/:id/userAge/:age", (req, res)=>{
+//     const id = req.params.id;
+//     const age = req.params.age;
+
+
+//     res.send(`<h1>Student id is: ${id}, age is: ${age}</h1>`)
+// })
+
+
+
+// Header Request
+
+// app.get("/", (req, res) => {
+//     const id = req.header('id');
+//     const name = req.header('name');
+//     res.send(`<h1>Student id is: ${id}, name is: ${name}</h1>`)
+// });
+
+
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+
+
+// Make post request with JSON data or form data
+
+app.post("/user", (req, res)=>{
+    const name = req.body.name;
+    const age = req.body.age;
+    res.send(`Welcome ${name}, you are ${age} years old.`)
+
+})
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
