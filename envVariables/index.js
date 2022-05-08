4ae0664cd4554d6708622da39d7d0d917436f0ec
faizulislamfair <faizulislamfair@gmail.com>
2022-05-08 10:40:27 +0600

    environment variables

---
+++ envVariables/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+
+
+app.get("/", (req, res)=>{
+    res.send("This is home route")
+})
+
+
+app.listen(PORT, ()=>{
+
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
